4dbe995aaff3b0cef49ad007758c34c169e06923
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-07-23 18:24:52 -0300

    feat: Exemplo cli

---
+++ src/main/cli/index.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include "index.h"
+#include "../entities/UserEntity.h"
+
+using namespace std;
+
+size_t welcome() {
+    size_t option;
+
+    cout << "     Bem-vindo ao BeTI     " << endl;
+    cout << "============================" << endl;
+    cout << "1) Efetuar Login" << endl;
+    cout << "2) Criar conta" << endl;
+    cout << "Escolha a sua opção: ";
+    cin >> option;
+    getchar();
+
+    return option;
+}
+
+UserEntity createAccount() {
+    string name;
+    string email;
+    string password;
+    double balance;
+
+    cout << "           Cadastrar Novo Usuário           " << endl;
+    cout << "============================================" << endl;
+    cout << "Nome: ";
+    getline(cin, name);
+    cout << "Email: ";
+    getline(cin, email);
+    cout << "Senha: ";
+    getline(cin, password);
+    cout << "Saldo inicial: ";
+    cin >> balance;
+
+    return UserEntity(0, name, email, password, balance);
+}
