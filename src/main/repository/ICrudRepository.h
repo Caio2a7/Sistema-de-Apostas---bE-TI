c2f1c4b901f1029f8611a8ede4e46e5a454ac039
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-07-25 18:46:07 -0300

    feat: Implementar classes para definir métodos básicos aos repositorys

---
+++ src/main/repository/ICrudRepository.h
@@ -0,0 +1,31 @@
+#ifndef ICRUDREPOSITORY_H
+#define ICRUDREPOSITORY_H
+
+#include <iostream>
+#include <pqxx/pqxx>
+#include <optional>
+#include <stdexcept>
+#include <sstream>
+#include <vector>
+#include <string>
+#include "./utils/QueryBuilder.h"
+#include "../enum/TablesDataBaseEnum.h"
+#include "../infra/db/configure.h"
+
+using namespace std;
+
+template <typename T>
+class ICrudRepository {
+public:
+    virtual ~ICrudRepository() = default;
+
+    virtual string getCodeAutoIncrementId(T* entity) = 0;
+    
+    virtual void save(pqxx::connection* conn, QueryMetaData *queryMetaData);
+    
+    optional<pqxx::result> findById(pqxx::connection* conn, QueryMetaData *queryMetaData, size_t id);
+
+    optional<pqxx::result> findAll(pqxx::connection* conn, QueryMetaData *queryMetaData);
+};
+
+#endif
