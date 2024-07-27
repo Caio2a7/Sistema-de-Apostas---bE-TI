d29a32ab517389f46f425f22e38d18c427f0a01c
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-07-26 23:37:49 -0300

    exemplo main

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,23 +2,41 @@
 #include <pqxx/pqxx>
 #include <vector>
 #include <optional>
+#include "service/EventService.h"
+#include "service/SportService.h"
 #include "service/ParticipantsService.h"
+#include "entities/EventEntity.h"
+#include "entities/SportEntity.h"
 #include "entities/ParticipantsEntity.h"
 #include "enum/UserRoleEnum.h"
+#include "enum/EventStatusEnum.h"
 
 int main() {
     try {
         // Criar uma conexão com o banco de dados
         pqxx::connection conn("dbname=beti user=postgres password=root hostaddr=127.0.0.1 port=5432");
-        ParticipantsService userService;
-        // ParticipantsEntity user(0, "usuario5", "usuario5@email.com", "123", UserRoleEnum::USUARIO, 100);
-        ParticipantsEntity entity(0, "Vasco", 2);
+        EventService userService;
+        SportService sportService;
+        ParticipantsService participantsService;
+        // EventEntity user(0, "usuario5", "usuario5@email.com", "123", UserRoleEnum::USUARIO, 100);
+        // SportEntity sport = sportService.findById(&conn, 2).value();
+        // ParticipantsEntity p1 = participantsService.findById(&conn, 1).value();
+        // ParticipantsEntity p2 = participantsService.findById(&conn, 2).value();
+        // std::array<double, 3> arr = {123.45, 678.90, 234.56};
+        // EventStatusEnum status = EventStatusEnum::AGENDADA;
 
-        // std::optional<ParticipantsEntity> userAuth = userService.authUser(&conn, "user1@email.com", "123456");
-        userService.save(&conn, &entity);
-        optional<vector<ParticipantsEntity>> userFindAll =  userService.findAll(&conn);
-        std::optional<ParticipantsEntity> userId = userService.findById(&conn, 1);
-        // std::optional<ParticipantsEntity> userEmail = userService.findByEmail(&conn, "user2@email.com");
+        // Para o horário conseguir ser salvo no banco de dados ele tem que seguir o padrão abaixo
+        // ano-mes-dia hora:minutos:segundos.centesimos
+        // 2024-07-26 23:20:02.818
+        // std::string horario = "2024-07-26 23:20:02.818";
+
+        // EventEntity entity(0, sport, p1, p2, arr, horario, status);
+
+        // std::optional<EventEntity> userAuth = userService.authUser(&conn, "user1@email.com", "123456");
+        // userService.save(&conn, &entity);
+        optional<vector<EventEntity>> userFindAll =  userService.findAll(&conn);
+        std::optional<EventEntity> userId = userService.findById(&conn, 1);
+        // std::optional<EventEntity> userEmail = userService.findByEmail(&conn, "user2@email.com");
 
         // cout << "UserAuth" << endl;
         // userAuth.value().toString();
