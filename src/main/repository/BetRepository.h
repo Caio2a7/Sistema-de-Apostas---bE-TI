ebd4f5b4fb9f09bf49091e34963cc714f4dd70dd
Rodrigo Nóbrega <rofen1@protonmail.com>
2024-07-28 20:18:13 -0300

    feat: Criar repository para os bets

---
+++ src/main/repository/BetRepository.h
@@ -0,0 +1,21 @@
+#ifndef BET_REPOSITORY_H
+#define BET_REPOSITORY_H
+
+#include <pqxx/pqxx>
+#include <vector>
+#include <array>
+#include <string>
+#include <sstream>
+#include <optional>
+#include "CrudRepositoryImpl.h"
+#include "../entities/BetEntity.h"
+#include "../infra/db/configure.h"
+
+using namespace std;
+
+class BetRepository : public CrudRepositoryImpl<BetEntity> {
+    public:
+        virtual ~BetRepository() = default;
+};
+
+#endif
